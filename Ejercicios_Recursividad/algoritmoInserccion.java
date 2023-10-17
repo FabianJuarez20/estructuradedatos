887046f34e3bf958d93f755d1890c8f6df57ee17
Fabian_Juarez <79298962+FabianJuarez20@users.noreply.github.com>
2023-10-17 22:57:41 +0000

    algoritmoInserccion

---
+++ Ejercicios_Recursividad/algoritmoInserccion.java
@@ -0,0 +1,12 @@
+public class algoritmoInserccion {
+    public static void main(String[] args) {
+        int[] arreglo = { 5, 2, 9, 1, 5, 6 };
+        int actual;
+
+        for (int i = 0; i < arreglo.length; i++) {
+            actual = arreglo[i];
+            System.out.println(actual);
+            
+        }
+    }
+}
--- Ejercicios_Recursividad/recursivoEjemplo.java
+++ Ejercicios_Recursividad/recursivoEjemplo.java
@@ -2,23 +2,24 @@ import java.util.Scanner;
 
 public class recursivoEjemplo {
     public static void main(String[] args) {
-        Scanner read = new Scanner(System.in);
-        System.out.println("Numero:");
-        int numero = read.nextInt();
-        System.out.println("Exponente:");
-        int exponente = read.nextInt();
-        System.out.println(exponencialRecursivo(numero, exponente));
+        try (Scanner read = new Scanner(System.in)) {
+            System.out.println("Numero:");
+            int numero = read.nextInt();
+            System.out.println("Exponente:");
+            int exponente = read.nextInt();
+            System.out.println(exponencialRecursivo(numero, exponente));
+        }
 
     }
 
-    private static int exponencialRecursivo(int dato,int exponencial) {
+    private static int exponencialRecursivo(int dato, int exponencial) {
         // condicion de paro
         int numero = dato;
         int exponente = exponencial;
 
-        if(exponente == 0){
+        if (exponente == 0) {
             return 1;
-        }else{
+        } else {
             numero = numero * exponencialRecursivo(numero, exponencial - 1);
             return numero;
         }
