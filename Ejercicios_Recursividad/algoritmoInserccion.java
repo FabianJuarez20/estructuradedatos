bff714e10266e25036478cbb303cf6f12748c701
Fabian_Juarez <79298962+FabianJuarez20@users.noreply.github.com>
2023-10-18 01:00:14 +0000

    cambios extras

--- Ejercicios_Recursividad/algoritmoInserccion.java
+++ Ejercicios_Recursividad/algoritmoInserccion.java
@@ -2,9 +2,13 @@ public class algoritmoInserccion {
     public static void main(String[] args) {
         int[] arreglo = { 5, 2, 9, 1, 5, 6 };
         int actual;
-
+        int j;
         for (int i = 0; i < arreglo.length; i++) {
             actual = arreglo[i];
+            for (j = i; j > 0 && arreglo[j - 1] > actual; j--) {
+            arreglo[j] = arreglo[j - 1];
+        }
+        arreglo[j] = actual;
             System.out.println(actual);
             
         }
